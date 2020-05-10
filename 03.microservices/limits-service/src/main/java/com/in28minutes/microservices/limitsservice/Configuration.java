8f6076b5cab9140e38bd5e823a339b68d51d8cea
malli1011 <mallikarjuna_bandi@epam.com>
2020-05-10 11:34:50 +0200

    Microservices upgrade

--- 03.microservices/limits-service/src/main/java/com/in28minutes/microservices/limitsservice/Configuration.java
+++ 03.microservices/limits-service/src/main/java/com/in28minutes/microservices/limitsservice/Configuration.java
@@ -6,24 +6,23 @@ import org.springframework.stereotype.Component;
 @Component
 @ConfigurationProperties("limits-service")
 public class Configuration {
-	
-	private int minimum;
-	private int maximum;
 
-	public void setMinimum(int minimum) {
-		this.minimum = minimum;
-	}
+    private int minimum;
+    private int maximum;
 
-	public void setMaximum(int maximum) {
-		this.maximum = maximum;
-	}
+    public int getMinimum() {
+        return minimum;
+    }
 
-	public int getMinimum() {
-		return minimum;
-	}
+    public void setMinimum(int minimum) {
+        this.minimum = minimum;
+    }
 
-	public int getMaximum() {
-		return maximum;
-	}
+    public int getMaximum() {
+        return maximum;
+    }
 
+    public void setMaximum(int maximum) {
+        this.maximum = maximum;
+    }
 }
